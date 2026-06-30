ca9f5aa0dbfad3ee160e6c70c5a90129adc73d28
Giorgio <giorgio291@gmail.com>
2026-06-30 12:59:19 +0200

    Revert "New feats" (#11)

--- js/frames/groupStandard-3.js
+++ js/frames/groupStandard-3.js
@@ -15,7 +15,6 @@ loadFramePacks([
 	{name:'"The List" Stamp', value:'TheList'},
 	{name:'Other Frames', value:'disabled'},
 	{name:'Lands', value:'M15Lands'},
-	{name:'Prepare (Secrets of Strixhaven)', value: 'Prepare'},
 	{name:'Stations', value:'StationRegular'},
 	{name:'Borderless Stations', value:'StationBorderless'},
 	{name:'Omens (Tarkir Dragonstorm)', value:'Omen'},
