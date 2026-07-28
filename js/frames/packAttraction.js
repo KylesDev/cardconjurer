551089c5284e6313cd19f9bd1165ef513e800781
Kyles <kyles@gabbro.lan>
2026-07-28 11:49:35 +0200

    feat(fork): register attraction transparent frame + thumb in pack

--- js/frames/packAttraction.js
+++ js/frames/packAttraction.js
@@ -3,6 +3,7 @@ var masks = [{src:'/img/frames/attraction/maskPinline.png', name:'Pinline'}, {sr
 //defines available frames
 availableFrames = [
 	{name:'Attraction Frame', src:'/img/frames/attraction/attraction.png', masks:masks},
+	{name:'Attraction Frame (Transparent)', src:'/img/frames/attraction/attractionTransparent.png', masks:masks},
 	{name:'Light 2', src:'/img/frames/attraction/2.png', bounds:{x:1362/1500, y:1443/2100, width:65/1500, height:65/2100}},
 	{name:'Light 3', src:'/img/frames/attraction/3.png', bounds:{x:1362/1500, y:1543/2100, width:65/1500, height:65/2100}},
 	{name:'Light 4', src:'/img/frames/attraction/4.png', bounds:{x:1362/1500, y:1643/2100, width:65/1500, height:65/2100}},
